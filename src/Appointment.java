5f47550cda7725aa4aef9b36ce0bd9c7bea6ced4
2608690050 <2608690050@qq.com>
2024-11-20 17:34:37 +0800

    Update Appointment.java

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,7 +1,7 @@
 public class Appointment {
     // 患者的详细信息
     private String patientName;
-    private String patientPhone;
+    private int patientPhone;
     
     // 患者首选的时间段
     private String preferredTimeSlot;
@@ -12,13 +12,13 @@ public class Appointment {
     // 默认构造函数
     public Appointment() {
         this.patientName = "";
-        this.patientPhone = "";
+        this.patientPhone = 0;
         this.preferredTimeSlot = "";
         this.selectedDoctor = null;
     }
 
     // 带参构造函数，用于初始化所有实例变量
-    public Appointment(String patientName, String patientPhone, String preferredTimeSlot, HealthProfessional selectedDoctor) {
+    public Appointment(String patientName, int patientPhone, String preferredTimeSlot, HealthProfessional selectedDoctor) {
         this.patientName = patientName;
         this.patientPhone = patientPhone;
         this.preferredTimeSlot = preferredTimeSlot;
@@ -38,5 +38,37 @@ public class Appointment {
         }
     }
 
-   
+    //方法：Getter 和 Setter
+    public String getpatientName() {
+        return patientName;
+    }
+
+    public void setpatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public int getpatientPhone() {
+        return patientPhone;
+    }
+
+    public void setSpecialization(int patientPhone) {
+        this.patientPhone = patientPhone;
+    }
+
+    public String getpreferredTimeSlot() {
+        return preferredTimeSlot;
+    }
+
+    public void setpreferredTimeSlot(String preferredTimeSlot) {
+        this.preferredTimeSlot = preferredTimeSlot;
+    }
+
+    public HealthProfessional selectedDoctor() {
+        return selectedDoctor;
+    }
+
+    public void selectedDoctor(HealthProfessional selectedDoctor) {
+        this.selectedDoctor = selectedDoctor;
+    }
+
 }
