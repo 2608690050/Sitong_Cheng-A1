b40587004d94b7a951dd5722a658b81836017844
2608690050 <2608690050@qq.com>
2024-11-20 19:59:35 +0800

    Update HealthProfessional.java
    Change the annotation from Chinese to English

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -1,31 +1,31 @@
 public class HealthProfessional {
-    // 实例变量
-    private int id;               //只包含数字
+    //Instance variable
+    private int id;               //Only contains numbers
     private String name;          
-    private int  yearsOfExperience;   //与医生类型无关
+    private int  yearsOfExperience;   //Not related to the type of doctor
     
-    // 默认构造函数
+    //Default constructor
     public HealthProfessional() {
         this.id = 0;
         this.name = "";
         this.yearsOfExperience = 0;
     }
     
-    // 初始化所有实例变量的第二个构造函数
+    //Initialize the second constructor of all instance variables
     public HealthProfessional(int id, String name, int yearsOfExperience) {
         this.id = id;
         this.name = name;
         this.yearsOfExperience = yearsOfExperience;
     }
     
-    // 打印所有实例变量的方法
+    //Method for printing all instance variables
     public void printDetails() {
         System.out.println("HealthProfessional ID: " + id);
         System.out.println("Name: " + name);
         System.out.println("YearsOfExperience: " + yearsOfExperience);
     }
     
-    //方法:Getter和Setter
+    //Method: Getter and Setter
     public int getId() {
         return id;
     }
