a9f448e7535de25fcb1e164d3f3d01014a78f9c4
2608690050 <2608690050@qq.com>
2024-11-20 11:56:51 +0800

    Update HealthProfessional.java

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -2,27 +2,27 @@ public class HealthProfessional {
     // 实例变量
     private int id;               //只包含数字
     private String name;          
-    private String description;   //与医生类型无关
+    private int  yearsOfExperience;   //与医生类型无关
     
     // 默认构造函数
     public HealthProfessional() {
         this.id = 0;
         this.name = "";
-        this.description = "";
+        this.yearsOfExperience = 0;
     }
     
     // 初始化所有实例变量的第二个构造函数
-    public HealthProfessional(int id, String name, String description) {
+    public HealthProfessional(int id, String name, int yearsOfExperience) {
         this.id = id;
         this.name = name;
-        this.description = description;
+        this.yearsOfExperience = yearsOfExperience;
     }
     
     // 打印所有实例变量的方法
     public void printDetails() {
         System.out.println("HealthProfessional ID: " + id);
         System.out.println("Name: " + name);
-        System.out.println("Description: " + description);
+        System.out.println("YearsOfExperience: " + yearsOfExperience);
     }
     
 }
